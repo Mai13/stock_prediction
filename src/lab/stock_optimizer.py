1b0e2182320379bfe0ca194d31532d17f57b0c84
Mai13 <maialen.ber@hotmail.com>
2020-04-02 22:36:17 +0200

    loading data

--- src/lab/stock_optimizer.py
+++ src/lab/stock_optimizer.py
@@ -1,5 +1,8 @@
 from data_loader import DataLoader
 from ticker_selector import ChooseTicker
+import logging
+
+logger = logging.getLogger('Stock Optimizer')
 
 
 class StockOptimizer:
@@ -10,5 +13,8 @@ class StockOptimizer:
         self.choose_ticker = ChooseTicker()
 
     def run(self):
+        logger.info('Select tickers')
         tickers = self.choose_ticker.random_tickers(self.number_of_tickers)
+        logger.info(f'The selected tickers are {tickers}')
         self.data_loader.load(tickers)
+        logger.info('Data for all the tickers is loaded')
