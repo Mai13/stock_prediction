faaf61d8746cf3bdf6070195eb53c0c7d2d4d0d4
Mai13 <maialen.ber@hotmail.com>
2020-04-26 20:59:25 +0200

    criterion deleted

--- src/lab/random_forest.py
+++ src/lab/random_forest.py
@@ -32,7 +32,6 @@ class RandomForest:
             validation,
             min_samples_leaf,
             max_features,
-            criterion,
             min_samples_split,
             n_estimators,
             max_depth):
@@ -43,7 +42,6 @@ class RandomForest:
         model = RandomForestRegressor(random_state=self.random_state,
                                       min_samples_leaf=min_samples_leaf,
                                       max_features=max_features,
-                                      # criterion=criterion,
                                       min_samples_split=min_samples_split,
                                       n_estimators=n_estimators,
                                       max_depth=max_depth)
@@ -58,7 +56,7 @@ class RandomForest:
 
         dump(
             model,
-            f'{self.model_path}/ticker_{self.ticker}_min_samples_leaf_{min_samples_leaf}_max_features_{max_features}_criterion_{criterion}'
+            f'{self.model_path}/ticker_{self.ticker}_min_samples_leaf_{min_samples_leaf}_max_features_{max_features}'
             f'_min_samples_split_{min_samples_split}_n_estimators_{n_estimators}_max_depth_{max_depth}.joblib')
 
         return mse_validation, mse_train
@@ -67,14 +65,13 @@ class RandomForest:
             self,
             min_samples_leaf,
             max_features,
-            criterion,
             min_samples_split,
             n_estimators,
             max_depth):
 
         model = load(
-            f'{self.model_path}/ticker_{self.ticker}_min_samples_leaf_{min_samples_leaf}_max_features_{max_features}_criterion_'
-            f'{criterion}_min_samples_split_{min_samples_split}_n_estimators_{n_estimators}_max_depth'
+            f'{self.model_path}/ticker_{self.ticker}_min_samples_leaf_{min_samples_leaf}_max_features_{max_features}_'
+            f'_min_samples_split_{min_samples_split}_n_estimators_{n_estimators}_max_depth'
             f'_{max_depth}.joblib')
         return model
 
@@ -83,7 +80,6 @@ class RandomForest:
             test,
             min_samples_leaf,
             max_features,
-            criterion,
             min_samples_split,
             n_estimators,
             max_depth):
@@ -91,7 +87,6 @@ class RandomForest:
         trained_model = self.__load_checkpoint(
             min_samples_leaf,
             max_features,
-            criterion,
             min_samples_split,
             n_estimators,
             max_depth)
@@ -122,50 +117,46 @@ class RandomForest:
                 'parameters').get('min_samples_leaf'):
             for max_features in model_parameters.get(
                     'parameters').get('max_features'):
-                for criterion in model_parameters.get(
-                        'parameters').get('criterion'):
-                    for min_samples_split in model_parameters.get(
-                            'parameters').get('min_samples_split'):
-                        for n_estimators in model_parameters.get(
-                                'parameters').get('n_estimators'):
-                            for max_depth in model_parameters.get(
-                                    'parameters').get('max_depth'):
-                                if model_parameters.get('training'):
-                                    logger.info(
-                                        f'Starts Training: min_samples_leaf {min_samples_leaf},'
-                                        f' max_features {max_features}, criterion {criterion},'
-                                        f' min_samples_split {min_samples_split}, n_estimators {n_estimators}'
-                                        f'max_depth {max_depth}')
-                                    mse_validation, mse_train = self.__train(train,
-                                                                             val,
-                                                                             min_samples_leaf=min_samples_leaf,
-                                                                             max_features=max_features,
-                                                                             criterion=criterion,
-                                                                             min_samples_split=min_samples_split,
-                                                                             n_estimators=n_estimators,
-                                                                             max_depth=max_depth)
-                                    logger.info(
-                                        f'MSE validation {mse_validation} and MSE train {mse_train}')
-                                else:
-                                    mse_validation, mse_train = None, None
-                                predictions, true_values = self.__test(
-                                    test, min_samples_leaf, max_features, criterion, min_samples_split,
-                                    n_estimators, max_depth)
-                                logger.info(f'Ends Test')
-                                # Todo: check overfitting with: train_loss,
-                                # val_loss
-                                current_mse = mean_squared_error(
-                                    true_values, predictions)
-                                if current_mse < mse:
-                                    best_parameters = {
-                                        'min_samples_leaf': min_samples_leaf,
-                                        'max_features': max_features,
-                                        'criterion': criterion,
-                                        'min_samples_split': min_samples_split,
-                                        'n_estimators': n_estimators,
-                                        'max_depth': max_depth
-                                    }
-                                    mse = current_mse
-                                percenatge_of_guess_in_trend = self.__get_trend(
-                                    true_values, predictions)
+                for min_samples_split in model_parameters.get(
+                        'parameters').get('min_samples_split'):
+                    for n_estimators in model_parameters.get(
+                            'parameters').get('n_estimators'):
+                        for max_depth in model_parameters.get(
+                                'parameters').get('max_depth'):
+                            if model_parameters.get('training'):
+                                logger.info(
+                                    f'Starts Training: min_samples_leaf {min_samples_leaf},'
+                                    f' max_features {max_features},'
+                                    f' min_samples_split {min_samples_split}, n_estimators {n_estimators}'
+                                    f'max_depth {max_depth}')
+                                mse_validation, mse_train = self.__train(train,
+                                                                         val,
+                                                                         min_samples_leaf=min_samples_leaf,
+                                                                         max_features=max_features,
+                                                                         min_samples_split=min_samples_split,
+                                                                         n_estimators=n_estimators,
+                                                                         max_depth=max_depth)
+                                logger.info(
+                                    f'MSE validation {mse_validation} and MSE train {mse_train}')
+                            else:
+                                mse_validation, mse_train = None, None
+                            predictions, true_values = self.__test(
+                                test, min_samples_leaf, max_features, min_samples_split,
+                                n_estimators, max_depth)
+                            logger.info(f'Ends Test')
+                            # Todo: check overfitting with: train_loss,
+                            # val_loss
+                            current_mse = mean_squared_error(
+                                true_values, predictions)
+                            if current_mse < mse:
+                                best_parameters = {
+                                    'min_samples_leaf': min_samples_leaf,
+                                    'max_features': max_features,
+                                    'min_samples_split': min_samples_split,
+                                    'n_estimators': n_estimators,
+                                    'max_depth': max_depth
+                                }
+                                mse = current_mse
+                            percenatge_of_guess_in_trend = self.__get_trend(
+                                true_values, predictions)
         return best_parameters, mse, percenatge_of_guess_in_trend
