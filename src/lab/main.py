9376f4c2d1786ed71fbcbf517883c39d77614707
Mai13 <maialen.ber@hotmail.com>
2020-05-05 22:52:25 +0200

    some new tries and future ideas

--- src/lab/main.py
+++ src/lab/main.py
@@ -5,8 +5,9 @@ import pathlib
 
 path = pathlib.Path(__file__).parent.absolute()
 logger = create_logger(f'{path.parent}/results', 'INFO')
-overfitting_threshold = 0.1  # 0.01, 0.05, 0.1, 0.5
-number_of_past_points = 7
+overfitting_threshold = 0.01  # 0.01, 0.05, 0.1, 0.5
+number_of_past_points = 7  # 1, 15
+# merece la pena poner number of steps ahead
 
 feed_forward = {
     'model': 'feed_forward_neural_net',
@@ -72,7 +73,7 @@ arima = {
 }
 
 # models = [arima]
-models = [xgboost, feed_forward]  # random_forest
+models = [random_forest, xgboost]  # random_forest, feed_forward
 # models = [random_forest]
 # models = [xgboost]
 
