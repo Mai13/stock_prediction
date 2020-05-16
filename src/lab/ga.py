9e0e1f88c9a94483649143aa09b4222bf4f8fbcb
Mai13 <maialen.ber@hotmail.com>
2020-05-16 19:17:59 +0200

    Starting to implement the GA

---
+++ src/lab/ga.py
@@ -0,0 +1,14 @@
+import deap
+
+"""
+The idea of this module is to get the fastest way the best results model
+"""
+
+
+class GeneticAlgorithm:
+    def __init__(self):
+        pass
+
+    def objective_function(self, model, parameters):
+        # return objective functio with certain parameters
+        pass
